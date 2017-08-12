6209b51dd3bb6a6d2dd9676a349e1263f284eaed
Gabriel Kazuo <kazuo.gabriel@gmail.com>
2017-08-12 18:19:22 -0300

    Retrieving data from APi

--- app/src/main/java/com/example/gabriel/weatherapp/activities/MainActivity.kt
+++ app/src/main/java/com/example/gabriel/weatherapp/activities/MainActivity.kt
@@ -6,7 +6,11 @@ import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import com.example.gabriel.weatherapp.adapters.ForecastListAdapter
 import com.example.gabriel.weatherapp.R
+import com.example.gabriel.weatherapp.data.Request
+import org.jetbrains.anko.doAsync
 import org.jetbrains.anko.find
+import org.jetbrains.anko.longToast
+import org.jetbrains.anko.uiThread
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,5 +33,13 @@ class MainActivity : AppCompatActivity() {
         val forecasList : RecyclerView = find(R.id.forecast_list)
         forecasList.layoutManager = LinearLayoutManager(this)
         forecasList.adapter = ForecastListAdapter(items)
+
+        val url = "http://api.openweathermap.org/data/2.5/forecast/daily?" +
+                "APPID=15646a06818f61f7b8d7823ca833e1ce&q=94043&mode=json&units=metric&cnt=7"
+
+        doAsync() {
+            Request(url).run()
+            uiThread { longToast("Request executada") }
+        }
     }
 }
