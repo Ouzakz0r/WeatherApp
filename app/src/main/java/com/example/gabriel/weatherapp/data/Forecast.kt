314ad1b64da8ab230294f472d01b9fff98cf8a36
Gabriel Kazuo <kazuo.gabriel@gmail.com>
2017-08-20 16:22:12 -0300

    data class Forecast

---
+++ app/src/main/java/com/example/gabriel/weatherapp/data/Forecast.kt
@@ -0,0 +1,9 @@
+package com.example.gabriel.weatherapp.data
+
+import java.util.*
+
+/**
+ * Created by Gabriel on 20/08/2017.
+ */
+
+data class Forecast(val date: Date, val temperatura: Float, val details: String)
