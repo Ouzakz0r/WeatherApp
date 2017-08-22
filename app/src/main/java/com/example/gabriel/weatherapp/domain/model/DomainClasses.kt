f19f40c1b7d527b7f88e6cdb0767444eb74a07ae
Gabriel Kazuo <kazuo.gabriel@gmail.com>
2017-08-21 23:04:05 -0300

    Operator Overloading 11.2

--- app/src/main/java/com/example/gabriel/weatherapp/domain/model/DomainClasses.kt
+++ app/src/main/java/com/example/gabriel/weatherapp/domain/model/DomainClasses.kt
@@ -4,5 +4,11 @@ package com.example.gabriel.weatherapp.domain.model
  * Created by Gabriel on 20/08/2017.
  */
 
-data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>)
+data class ForecastList(val city: String, val country: String, val dailyForecast: List<Forecast>){
+
+    val size: Int get() = dailyForecast.size
+
+    operator fun get(position: Int): Forecast = dailyForecast[position]
+
+}
 data class Forecast(val date: String, val description: String, val high: Int, val low: Int)
